0f7af3053dc8cf47ac15fc76e2c7556045e47e24
rafael <rafael.rigoo@gmail.com>
2021-05-18 19:21:05 +0100

    Adicionando Participacao Especial atraves da Migration

---
+++ Friends/Migrations/20210517200648_seed-participacaoespecial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace Friends.Migrations
+{
+    public partial class seedparticipacaoespecial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Elenco", new string[] { "SeriadoInfoId", "Discriminator", "NomeReal", "Nacionalidade", "DatadeNascimento", "QuantidadedeFilhos", "Personagem"},
+               new object[] { 1, "ParticipacaoEspecial", "Maggie Wheeler", "Los Angeles, California, U.S.", new DateTime(1969, 02, 11), 0, " Janice Goralnik" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
