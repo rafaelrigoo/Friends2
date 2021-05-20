98a8509fa8d4f36befe3ac4631a9a65a12ca6a29
rafael <rafael.rigoo@gmail.com>
2021-05-20 22:30:45 +0100

    adicionando nova coluna na minha tabela

--- Friends/Migrations/20210520185116_adicionando-propriedade-numeroGeral.Designer.cs
+++ Friends/Migrations/20210520185116_adicionando-propriedade-numeroGeral.Designer.cs
@@ -4,14 +4,16 @@ using Friends.Dados;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Friends.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20210520185116_adicionando-propriedade-numeroGeral")]
+    partial class adicionandopropriedadenumeroGeral
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Friends/Migrations/20210520185116_adicionando-propriedade-numeroGeral.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Friends.Migrations
+{
+    public partial class adicionandopropriedadenumeroGeral : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "numeroEpisodionaTemporada",
+                table: "Episodios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "numeroEpisodionoGeral",
+                table: "Episodios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "numeroEpisodionaTemporada",
+                table: "Episodios");
+
+            migrationBuilder.DropColumn(
+                name: "numeroEpisodionoGeral",
+                table: "Episodios");
+        }
+    }
+}
