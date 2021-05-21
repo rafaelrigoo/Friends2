fbe90a8c0fffdfdef4fbb1ac3fd2c1ff9abdfb46
rafael <rafael.rigoo@gmail.com>
2021-05-21 10:38:49 +0100

    Adcionando propriedade na classe e usando ela na View

---
+++ Friends/Migrations/20210521090215_adicionando-propriedade-CaminhoImagem2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Friends.Migrations
+{
+    public partial class adicionandopropriedadeCaminhoImagem2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CaminhoImagem2",
+                table: "Elenco",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CaminhoImagem2",
+                table: "Criadores",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CaminhoImagem2",
+                table: "Elenco");
+
+            migrationBuilder.DropColumn(
+                name: "CaminhoImagem2",
+                table: "Criadores");
+        }
+    }
+}
